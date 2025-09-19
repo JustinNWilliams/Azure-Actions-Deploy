95137aee706df5269cdc282aea948d63449f9218
Justin Williams <justinnwilliams4@gmail.com>
2025-09-19 02:05:26 -0700

    First Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from GitHub Actions! This app was deployed automatically.');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
